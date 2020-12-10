2e76f8f2f729975e5c0c2b9362ee874485438046
kalwalt <github@kalwaltart.it>
2020-12-10 23:13:22 +0100

    trying printing refData

--- lib/SRC/WebARKitTrackers/WebARKitOpticalTracking/WebARKitOrbTracker.cpp
+++ lib/SRC/WebARKitTrackers/WebARKitOpticalTracking/WebARKitOrbTracker.cpp
@@ -13,6 +13,7 @@ void WebARKitOrbTracker::initialize(uchar refData[], size_t refCols, size_t refR
     std::cout << "Orb created!" << std::endl;
     matcher = cv::BFMatcher::create();
     std::cout << "BFMatcher created!" << std::endl;
+    std::cout << refData << std::endl;
     cv::Mat refGray = im_gray(refData, refCols, refRows);
     std::cout << "Gray Image!" << std::endl;
     orb->detectAndCompute(refGray, cv::noArray(), refKeyPts, refDescr);
