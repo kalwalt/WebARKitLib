6f79959f5ad71d570c8d6f5cc362d882ac2d476e
kalwalt <github@kalwaltart.it>
2022-05-30 18:33:50 +0200

    removing old DEFINEs

--- include/WebARKitTrackers/WebARKitOpticalTracking/WebARKitConfig.h
+++ include/WebARKitTrackers/WebARKitOpticalTracking/WebARKitConfig.h
@@ -1,6 +1,6 @@
 #ifndef WEBARKIT_CONFIG_H
 #define WEBARKIT_CONFIG_H
-#define GOOD_MATCH_RATIO    0.7f
-#define MAX_FEATURES        2000
-#define N                   10
+extern const double GOOD_MATCH_RATIO;
+extern const int MAX_FEATURES;
+extern const int N;
 #endif
---
+++ lib/SRC/WebARKitTrackers/WebARKitOpticalTracking/WebARKitConfig.cpp
@@ -0,0 +1,5 @@
+#include <WebARKitTrackers/WebARKitOpticalTracking/WebARKitConfig.h>
+
+extern const double GOOD_MATCH_RATIO  = 0.7f;
+extern const int MAX_FEATURES = 2000;
+extern const int N = 10;
